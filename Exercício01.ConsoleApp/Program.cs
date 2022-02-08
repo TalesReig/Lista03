0edb3f9b1393ebaaecc8ec194c186860edd08f8a
TalesReigCordova <takesmohamed@gmail.com>
2022-02-08 15:04:50 -0200

    Adicionar arquivos de projeto.

---
+++ Exercício01.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercício01.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
