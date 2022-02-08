1da8129e5bbee6a9ffb22c3561728f2cc8a4c6de
TalesReigCordova <takesmohamed@gmail.com>
2022-02-08 15:30:44 -0200

    Questã01 concluida

--- Exercício01.ConsoleApp/Program.cs
+++ Exercício01.ConsoleApp/Program.cs
@@ -6,7 +6,23 @@ namespace Exercício01.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double MaiorAltura = 0, MenorAltura = 1000,Altura;
+
+            for (int i = 0; i < 3; i++){
+                Console.Write("Digite a altura ( em metros ) da pessoa da posição " + i + ": ");
+                Altura = Convert.ToDouble(Console.ReadLine());
+
+                if (Altura > MaiorAltura) {
+                    MaiorAltura = Altura;
+                }
+                if (Altura < MenorAltura)
+                {
+                    MenorAltura = Altura;
+                }
+            }
+
+            Console.WriteLine("\n A maior altura é " + MaiorAltura + "m e a menor altura é " + MenorAltura + "m.");
+            Console.ReadLine();
         }
     }
 }
