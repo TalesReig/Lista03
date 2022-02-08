43e7bd91a2c1bf6df4e0f689d9084441fe9dc539
TalesReigCordova <takesmohamed@gmail.com>
2022-02-08 15:46:30 -0200

    Questão 02 Concluida

---
+++ Exercício02.ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercício02.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numeros ímpares entre 100 e 200:");
+
+            for(int i = 0; i < 100; i++)
+            {
+                int numero = 100 + i;
+                int teste = numero % 2;
+
+                if(teste != 0)
+                {
+                    Console.Write(numero+", ");
+                }
+
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
