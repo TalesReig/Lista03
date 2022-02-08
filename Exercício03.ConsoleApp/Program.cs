ab44135550c2eb06951ad5f11e90fa5185ebcae8
TalesReigCordova <takesmohamed@gmail.com>
2022-02-08 16:08:25 -0200

    Questão 03 concluida

---
+++ Exercício03.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercício03.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soma de todos os números ímpares que são múltiplos de três " +
+            "e que se encontram no conjunto dos números de 1 até 500.");
+
+            for (int i = 0; i < 500; i++)
+            {
+                int numero = 0 + i;
+                int teste = numero % 2;
+                int teste2 = numero % 3;
+
+                if (teste != 0 && teste2 == 0 )
+                {
+                    Console.Write(numero + ", ");
+                }
+            }
+        }
+    }
+}
